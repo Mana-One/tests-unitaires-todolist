0c35a0b9613bde1dcd90592f9d91710373355198
Mana-One <paolo.manaois@yahoo.fr>
2021-04-29 18:56:04 +0200

    added ToDoList entity

--- src/entities/Item.ts
+++ src/entities/Item.ts
@@ -13,6 +13,14 @@ export class Item {
         private createdAt: Date
     ){}
 
+    public getName(): string {
+        return this.name;
+    }
+
+    public getCreatedAt(): Date {
+        return this.createdAt;
+    }
+
     public static build(props: ItemProps): Result<Item> {
         if(props.name.length === 0){
             return Result.ko<Item>("Name too short");
